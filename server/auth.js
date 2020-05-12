9764e54a6c3e147b2e3390e47cf199a01610eb28
Dalton Curtin <daltoncurtin@gmail.com>
2020-05-12 09:36:31 -0400

    test

--- server/auth.js
+++ server/auth.js
@@ -86,8 +86,8 @@ module.exports = function(models) {
   function login(req, res, next) {
 
     var url = 'https://cors-anywhere.herokuapp.com/https://test.salesforce.com/services/oauth2/authorize?response_type=token&client_id=3MVG9ahGHqp.k2_wp5KNZXDK5mBqaJaRv6ss6l7gQkGLZfriwyGa_1aRXE88g0W5oT9rwlJQ31ieo52ucBrJm&redirect_uri=https://test-m-mobile-midland.herokuapp.com/sfauth'
-    var x = new XMLHttpRequest();
-    x.open(GET,url);
+    //var x = new XMLHttpRequest();
+    //x.open(GET,url);
 
     //res.send(url);
     res.redirect(301,url);

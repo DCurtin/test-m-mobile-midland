fedb2a0db7cbd5c7330fc69ed5642b2b3c06061c
Dalton Curtin <daltoncurtin@gmail.com>
2020-05-11 22:36:40 -0400

    Test

--- server/auth.js
+++ server/auth.js
@@ -84,6 +84,10 @@ module.exports = function(models) {
   }
 
   function login(req, res, next) {
+
+    var url = 'https://test.salesforce.com/services/oauth2/authorize?response_type=token&client_id=3MVG9ahGHqp.k2_wp5KNZXDK5mBqaJaRv6ss6l7gQkGLZfriwyGa_1aRXE88g0W5oT9rwlJQ31ieo52ucBrJm&redirect_uri=https://test-m-mobile-midland.herokuapp.com/sfauth'
+    res.send(url);
+    /*
     var user = req.body;
 
     new models.User({
@@ -113,7 +117,7 @@ module.exports = function(models) {
           return res.status(401).send("Invalid Credentials");
         }
       });
-    });
+    });*/
   }
 
   function on_register(callback) {

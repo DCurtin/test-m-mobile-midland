c08b8b93f3cf50de131016c0d7fb416f9d8990cf
Dalton Curtin <daltoncurtin@gmail.com>
2020-05-12 13:56:45 -0400

    test

--- server/auth.js
+++ server/auth.js
@@ -79,7 +79,7 @@ module.exports = function(models) {
     var data = req.body;
     var accessToken = req.headers['access_token'];
     var refreshToken = req.headers['refresh_token'];
-    res.json(JSON.stringify(Object.keys(req)))
+    res.json(req._parsedUrl + req._parsedOriginalUrl)
     console.log("data: " + data + " accessToken: " + accessToken + " refToken: " + refreshToken );
   }
 

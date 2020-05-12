243b90b5ca86740e20d4a5c1f8e3343c2e365179
Dalton Curtin <daltoncurtin@gmail.com>
2020-05-11 21:53:06 -0400

    test

--- server.js
+++ server.js
@@ -38,17 +38,16 @@ app.use(express.static(path.join(__dirname, 'server/pages')));
 // Logging
 app.use(function(req, res, next) {
   logger.debug(req.method, req.url);
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
   next();
 });
 
 app.use(function(err, req, res, next) {
   logger.error(err.stack);
   res.status(500).send(err.message);
-});
-
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", '*');
-  next();
 });
 
 /********************* ROUTES *****************************/

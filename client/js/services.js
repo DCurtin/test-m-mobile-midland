519bc3553198d6b8c9fbadbf906444234631bc80
Dalton Curtin <daltoncurtin@gmail.com>
2020-05-12 11:52:13 -0400

    test

--- client/js/services.js
+++ client/js/services.js
@@ -82,15 +82,15 @@ angular.module('starter.services', [])
       var url2 = 'https://test.salesforce.com/services/oauth2/authorize?response_type=token&client_id=3MVG9ahGHqp.k2_wp5KNZXDK5mBqaJaRv6ss6l7gQkGLZfriwyGa_1aRXE88g0W5oT9rwlJQ31ieo52ucBrJm&redirect_uri=https://test-m-mobile-midland.herokuapp.com/sfauth'
       var url = '/login'
       var xhr = new XMLHttpRequest();
-      xhr.open('POST',corsallow+url2, true);
+      /*xhr.open('POST',corsallow+url2, true);
       xhr.withCredentials = false;
       xhr.setRequestHeader('Access-Control-Request-Headers', 'origin, x-requested-with');
       xhr.setRequestHeader('origin', ' https://test-m-mobile-midland.herokuapp.com');
-      xhr.send();
+      xhr.send();*/
       //var url = 'https://test.salesforce.com/services/oauth2/authorize?response_type=token&client_id=3MVG9ahGHqp.k2_wp5KNZXDK5mBqaJaRv6ss6l7gQkGLZfriwyGa_1aRXE88g0W5oT9rwlJQ31ieo52ucBrJm&redirect_uri=https://test-m-mobile-midland.herokuapp.com/sfauth'
-      /*return $http.post(url2).then(function(Result){
+      return $http.post(corsallow+url2).then(function(Result){
         console.log('test login');
-      });*/
+      });
       /*return $http.post('https://test-m-mobile-midland.herokuapp.com/#/login', {
         email: email,
         password: password

06ef8cb68d666e0b970f927d116ccb53d5f75faf
Dalton Curtin <daltoncurtin@gmail.com>
2020-05-12 15:37:35 -0400

    test

--- server.js
+++ server.js
@@ -11,8 +11,8 @@ var http           = require('http'),
     notifier       = require('./server/notifier'),
     restful        = require('./server/bookshelf_rest'),
     auth           = require('./server/auth')(models),
-    force          = require('./server/force');
-    rp             = require('request-promise');
+    force          = require('./server/force'),
+    rp             = require('request-promise'),
     request        = require('request');
     ;
 

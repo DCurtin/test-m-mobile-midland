fcfa6373f87f84b239e7257505e0ff6b336a6b75
Dalton Curtin <daltoncurtin@gmail.com>
2020-05-12 14:44:56 -0400

    test

--- server/auth.js
+++ server/auth.js
@@ -77,11 +77,10 @@ module.exports = function(models) {
 
   function sfAuth(req, res, next) {
     var data = req.body;
-    var accessToken = req.headers['access_token'];
-    var refreshToken = req.headers['refresh_token'];
-    //res.json(JSON.stringify(req._parsedUrl._raw) + JSON.stringify(req._parsedOriginalUrl)+ JSON.stringify(req.res)+ JSON.stringify(req.client)+ JSON.stringify(req.trailers)+ JSON.stringify(req.complete))
-    res.json('test')
-    console.log("data: " + data + " accessToken: " + accessToken + " refToken: " + refreshToken );
+    var code = req.code
+
+    res.json(code);
+
   }
 
   function login(req, res, next) {

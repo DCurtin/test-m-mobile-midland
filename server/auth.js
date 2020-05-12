796e3d3255a306b50bce4dba0b96f89126b3f6bf
Dalton Curtin <daltoncurtin@gmail.com>
2020-05-12 16:43:32 -0400

    test

--- server/auth.js
+++ server/auth.js
@@ -86,11 +86,12 @@ module.exports = function(models) {
     var url  = 'https://test.salesforce.com/services/oauth2/token'+'?grant_type=authorization_code&code='+ queryCode+'&client_id='+clientId+'&client_secret='+clientSec+'&redirect_uri='+redirUrl+'&state=token';
     var rp  = require('request-promise');
     //res.redirect(url);
-    res.json('test');
+    //res.json('test');
     rp(url).then( function(result){
       //res.json('test2');
       console.log(result);
       res.json('test2');
+      
     }).catch(function(err) {
       res.status(500).send(err.message);
     });

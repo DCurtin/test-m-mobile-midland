ff870f7bba38784991fe02d0d3616437e2edaf82
Dalton Curtin <daltoncurtin@gmail.com>
2020-05-13 14:01:03 -0400

    test

--- server/auth.js
+++ server/auth.js
@@ -92,6 +92,7 @@ module.exports = function(models) {
       var data = JSON.parse(result);
       var sfIdParts = data.id.split('/');
       var sfId = sfIdParts[sfIdParts.length - 1];
+      console.log()
       //res.json({access_token: data.access_token, id: data.id});
       console.log('res: ' + result);
       
@@ -99,6 +100,7 @@ module.exports = function(models) {
         //console.log(returnSfUser.attributes.email)
         //returnSfUser.set_title('test title');
         new models.account({dedicated_rep__c: sfId}).fetch().then(function(returnedAccount){
+          console.log(JSON.stringify(returnedAccount));
           var updateAccount = returnedAccount[Math.floor(Math.random() * (returnedAccount.length -1))];
           updateAccount.set_phone('7698893954');
           res.json(returnSfUser.attributes.email);

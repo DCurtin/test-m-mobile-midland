4232250fff41002281622e1b3db8e3507f1b1eae
Dalton Curtin <daltoncurtin@gmail.com>
2020-05-13 21:13:32 -0400

    test

--- client/js/services.js
+++ client/js/services.js
@@ -91,7 +91,7 @@ angular.module('starter.services', [])
         $window.sessionStorage.email = value.email;
         $window.localStorage.token = value.token;
 
-        $window.location.hash =''
+        //$window.location.hash =''
       }).catch(function(err){
         console.log('error: ' + JSON.stringify(err));
       })

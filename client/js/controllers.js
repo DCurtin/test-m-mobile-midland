482a56402be14837cee1aabba0a156e513239c0a
Dalton Curtin <daltoncurtin@gmail.com>
2020-05-14 16:43:45 -0400

    test

--- client/js/controllers.js
+++ client/js/controllers.js
@@ -238,13 +238,72 @@ angular.module('starter.controllers', [])
 
   $scope.getRandomFile = function(){
     RegistrationService.getFile("068g0000001dVZRAA2").then(function(resultFile){
-      //var decoder = new TextDecoder('windows-1252')  
-      var decoder = new TextDecoder() 
+      var decoder = new TextDecoder('windows-1252')  
+      //var decoder = new TextDecoder();
       var base64String;
       var decodedString;
       var reader = new FileReader();
       var textBlob;
       var fileurl;
+      var Base64Binary = {
+        _keyStr : "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",
+        
+        /* will return a  Uint8Array type */
+        decodeArrayBuffer: function(input) {
+          var bytes = (input.length/4) * 3;
+          var ab = new ArrayBuffer(bytes);
+          this.decode(input, ab);
+          
+          return ab;
+        },
+      
+        removePaddingChars: function(input){
+          var lkey = this._keyStr.indexOf(input.charAt(input.length - 1));
+          if(lkey == 64){
+            return input.substring(0,input.length - 1);
+          }
+          return input;
+        },
+      
+        decode: function (input, arrayBuffer) {
+          //get last chars to see if are valid
+          input = this.removePaddingChars(input);
+          input = this.removePaddingChars(input);
+      
+          var bytes = parseInt((input.length / 4) * 3, 10);
+          
+          var uarray;
+          var chr1, chr2, chr3;
+          var enc1, enc2, enc3, enc4;
+          var i = 0;
+          var j = 0;
+          
+          if (arrayBuffer)
+            uarray = new Uint8Array(arrayBuffer);
+          else
+            uarray = new Uint8Array(bytes);
+          
+          input = input.replace(/[^A-Za-z0-9\+\/\=]/g, "");
+          
+          for (i=0; i<bytes; i+=3) {	
+            //get the 3 octects in 4 ascii chars
+            enc1 = this._keyStr.indexOf(input.charAt(j++));
+            enc2 = this._keyStr.indexOf(input.charAt(j++));
+            enc3 = this._keyStr.indexOf(input.charAt(j++));
+            enc4 = this._keyStr.indexOf(input.charAt(j++));
+        
+            chr1 = (enc1 << 2) | (enc2 >> 4);
+            chr2 = ((enc2 & 15) << 4) | (enc3 >> 2);
+            chr3 = ((enc3 & 3) << 6) | enc4;
+        
+            uarray[i] = chr1;			
+            if (enc3 != 64) uarray[i+1] = chr2;
+            if (enc4 != 64) uarray[i+2] = chr3;
+          }
+        
+          return uarray;	
+        }
+      }
 
       /*reader.addEventListener("load", ()=>{
         window.open(reader.result);
@@ -255,9 +314,11 @@ angular.module('starter.controllers', [])
       $file = resultFile.data
 
       console.log($file.versiondata);
-      base64String = decoder.decode(new Uint8ClampedArray($file.versiondata));
+      base64String = decoder.decode(new Uint8Array($file.versiondata));
       console.log(base64String);
-      decodedString = atob(base64String);
+      //decodedString = atob(base64String);
+      decodedString = Base64Binary.decodeArrayBuffer(base64String);
+      decoder.decode(decodedString);
       //decodedString = new Buffer(base64String, 'base64').toString('ascii');
       console.log(decodedString);
 

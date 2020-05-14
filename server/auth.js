fae71b393d151f1004b4adced93a9b6cfd18d8df
Dalton Curtin <daltoncurtin@gmail.com>
2020-05-14 11:00:43 -0400

    test

--- server/auth.js
+++ server/auth.js
@@ -91,8 +91,8 @@ module.exports = function(models) {
     var dataBody = req.body;
     var code = dataBody.token;
 
-    var clientId='3MVG9ahGHqp.k2_wp5KNZXDK5mBqaJaRv6ss6l7gQkGLZfriwyGa_1aRXE88g0W5oT9rwlJQ31ieo52ucBrJm';
-    var clientSec='ACD35B3FC6AC4F4F1EF425489C36013EFD91F0831D3C24D6C889CF02C34EF88A';
+    var clientId = '3MVG9ahGHqp.k2_wp5KNZXDK5mBqaJaRv6ss6l7gQkGLZfriwyGa_1aRXE88g0W5oT9rwlJQ31ieo52ucBrJm';
+    var clientSec = process.env.App_Secret;
     var redirUrl= 'https://test-m-mobile-midland.herokuapp.com/sfauth';
     var url  = 'https://test.salesforce.com/services/oauth2/token'+'?grant_type=authorization_code&code='+ code+'&client_id='+clientId+'&client_secret='+clientSec+'&redirect_uri='+redirUrl+'&state=token';
     var rp  = require('request-promise');

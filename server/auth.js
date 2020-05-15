c9d7c480bea4f7d3c2afa780fc32d26185f42a82
Dalton Curtin <daltoncurtin@gmail.com>
2020-05-15 12:15:30 -0400

    test

--- server/auth.js
+++ server/auth.js
@@ -310,7 +310,7 @@ module.exports = function(models) {
     //var decoder = new StringDecoder('windows-1252')  
     
     models.contentVersion.query(function(qb) {
-      qb.where('sfid', '<>', givenSfid)
+      qb.where('sfid', '=', givenSfid)
     }).fetch({debug: true}).then(function(result){
       console.log('data: ' + Object.keys(result))
       console.log('data id: ' + result.id)

3d68e4abb596de008198ea97f6ee3245fdaf871f
Dalton Curtin <daltoncurtin@gmail.com>
2020-05-15 12:18:34 -0400

    test

--- server/auth.js
+++ server/auth.js
@@ -311,12 +311,9 @@ module.exports = function(models) {
     
     models.contentVersion.query(function(qb) {
       qb.where('sfid', '=', givenSfid)
-    }).fetch({debug: true}).then(function(result){
-      console.log('data: ' + Object.keys(result))
-      console.log('data id: ' + result.id)
-      console.log('data att: ' + result.get('sfid'))
+    }).fetch().then(function(result){
       //base64String = decoder.decode(new Uint8Array($result.versiondata));
-      base64String = (new Buffer(new Uint8Array(result.versiondata))).toString('utf-8');
+      base64String = (new Buffer(new Uint8Array(result.get('versiondata')))).toString('utf-8');
       base64Array = Base64Binary.decodeArrayBuffer(base64String);
       decodedString = (new Buffer(new Uint8Array(base64Array))).toString('utf-8');
       textBlob = new Blob([decodedString], {type: 'application/pdf'});

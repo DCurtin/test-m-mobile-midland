1385724823431da115f64840039ef69a421e3ec2
Dalton Curtin <daltoncurtin@gmail.com>
2020-05-15 17:07:41 -0400

    test

--- server/auth.js
+++ server/auth.js
@@ -318,6 +318,7 @@ module.exports = function(models) {
       base64String = result.get('versiondata').toString('utf-8');
       console.log(base64String)
       base64Array = Base64Binary.decodeArrayBuffer(base64String);
+      console.log(base64Array);
       //console.log(base64Array)
       //decodedString = (new Buffer(new Uint8Array(base64Array))).toString('ascii');
       //console.log(decodedString)

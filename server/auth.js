51aa7b4c98d645d40c76188e7cb404ef81e146aa
Dalton Curtin <daltoncurtin@gmail.com>
2020-05-16 20:37:34 -0400

    test

--- server/auth.js
+++ server/auth.js
@@ -247,7 +247,7 @@ module.exports = function(models) {
     var form = new formidable.IncomingForm();
     form.parse(req, function(err, fields, files){
       console.log(files);
-      console.log(files.bytes)
+      console.log(Object.keys(files))
       //console.log(fields);
       //console.log(req.body)
     })

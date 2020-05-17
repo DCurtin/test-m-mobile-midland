d1aba837c2db935803be06a3a705c28638f78b76
Dalton Curtin <daltoncurtin@gmail.com>
2020-05-17 12:52:36 -0400

    test

--- server/auth.js
+++ server/auth.js
@@ -327,7 +327,7 @@ module.exports = function(models) {
         //console.log(new Buffer(data));
         //console.log(data.toString('base64'));
         
-        new models.contentVersion({   versiondata: new Buffer('test1234'),
+        new models.contentVersion({   versiondata: new Buffer('test1234', 'base64'),
                                   pathonclient: files.file.path,
                                   title:'uploaded file.png',
                                   //fileextension: 'png',

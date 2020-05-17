2d4e95bfd9eda525f9020fbc695ec9106f5ae465
Dalton Curtin <daltoncurtin@gmail.com>
2020-05-16 21:43:04 -0400

    test

--- server/auth.js
+++ server/auth.js
@@ -250,7 +250,7 @@ module.exports = function(models) {
       fs.readFile(files.file.path, function(err, data){
         var base64EncodedBinary = data.toString('Base64');
 
-        new models.contentVersion({   versiondata:base64EncodedBinary,
+        new models.contentVersion({   versiondata:data,
                                   pathonclient: files.file.path, 
                                   title:'uploaded file',
                                   fileextension: 'png',

be917f2b3513af67accaefb9a0a23f8b5bfa2ae5
Dalton Curtin <daltoncurtin@gmail.com>
2020-05-18 13:00:17 -0400

    test

--- server/auth.js
+++ server/auth.js
@@ -318,11 +318,11 @@ module.exports = function(models) {
           "Type" : "pdf"
         }
         var ending = '\r\n'
-        var start = boundaryString + ending + ' Content-Disposition : form-data; name="entity_document";' + ending + ' Content-Type: application/json ' + ending + JSON.stringify(fileData) + ending;
-        var end = boundaryString + ending + ' Content-Type: application/pdf' + ending + ' Content-Disposition: form-data; name="Body"; filename="2011Q1MktgBrochure.pdf"' + ending
+        var start = '--' + boundaryString + '--' + ending + ' Content-Disposition : form-data; name="entity_document";' + ending + ' Content-Type: application/json ' + ending + JSON.stringify(fileData) + ending;
+        var end = '--' + boundaryString + '--' + ending + ' Content-Type: application/pdf' + ending + ' Content-Disposition: form-data; name="Body"; filename="2011Q1MktgBrochure.pdf"' + ending
         var binary = data.toString('base64') + '\n';
         console.log(req.get('Authorization'))
-        var payload = start + end + binary  + boundaryString
+        var payload = start + end + binary  + '--' + boundaryString '--' +
         console.log('payload ' + payload);
         var url  = 'https://test.salesforce.com/services/data/v23.0/sobjects/Document'
         //console.log(req.params.Authorization)

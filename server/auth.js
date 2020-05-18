ba8f2b2c690c6bcfafb74d6ec95f38453a94749a
Dalton Curtin <daltoncurtin@gmail.com>
2020-05-18 13:49:55 -0400

    test

--- server/auth.js
+++ server/auth.js
@@ -324,7 +324,7 @@ module.exports = function(models) {
         console.log(req.get('Authorization'))
         var payload = start + end + binary  + '--' + boundaryString + '--'
         console.log('payload ' + payload);
-        var url  = 'https://test.salesforce.com/services/data/v23.0/sobjects/Document'
+        var url  = 'test.salesforce.com/services/data/v23.0/sobjects/Document'
         //console.log(req.params.Authorization)
         var options = {
           uri: url,
